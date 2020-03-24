050b7d2dd5f862ddb6b81bee08dc6ac968e96573
jiaRuiXie <jiarui.xie@isima.fr>
2020-03-24 20:20:10 +0100

    'finishTestPriorityQueue'

--- jrxcppstl/xqueue.cpp
+++ jrxcppstl/xqueue.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-#include "xqueue.h"
-
-void print(jrxstl::queue<int> temp) {
-
-	while (!temp.empty()) {
-		std::cout << temp.front() << " ";
-		temp.pop();
-	}
-	std::cout << std::endl;
-}
-
-int main() {
-	jrxstl::queue<int> stc;
-	stc.push(3);
-	stc.push(4);
-	stc.push(2);
-	stc.push(5);
-	stc.push(7);
-	std::cout << "After push: ";
-	print(stc);
-
-	std::cout << "The size of queue: " << stc.size() << std::endl;
-
-	stc.pop();
-	stc.pop();
-	std::cout << "After pop: ";
-	print(stc);
-
-	std::cout << "The size of queue after pop: " << stc.size() << std::endl;
-
-	std::cout << "The front of queue: " << stc.front() << std::endl;
-	std::cout << "The back of queue: " << stc.back() << std::endl;
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include "xqueue.h"
+//
+//void print(jrxstl::queue<int> temp) {
+//
+//	while (!temp.empty()) {
+//		std::cout << temp.front() << " ";
+//		temp.pop();
+//	}
+//	std::cout << std::endl;
+//}
+//
+//int main() {
+//	jrxstl::queue<int> stc;
+//	stc.push(3);
+//	stc.push(4);
+//	stc.push(2);
+//	stc.push(5);
+//	stc.push(7);
+//	std::cout << "After push: ";
+//	print(stc);
+//
+//	std::cout << "The size of queue: " << stc.size() << std::endl;
+//
+//	stc.pop();
+//	stc.pop();
+//	std::cout << "After pop: ";
+//	print(stc);
+//
+//	std::cout << "The size of queue after pop: " << stc.size() << std::endl;
+//
+//	std::cout << "The front of queue: " << stc.front() << std::endl;
+//	std::cout << "The back of queue: " << stc.back() << std::endl;
+//	system("pause");
+//	return 0;
+//}
